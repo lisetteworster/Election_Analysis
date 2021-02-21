35e259505030375e769e683fa7f758317280bc15
lisetteworster Git Hub <lworster@me.com>
2021-02-21 14:59:21 -0800

    adding election results.

--- PyPoll.py
+++ PyPoll.py
@@ -80,10 +80,3 @@ now = datetime.datetime.now()
 # Print the present time.
 print("The time right now is ", now)
 
-# 1. The total number of votes cast
-
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote.
-
